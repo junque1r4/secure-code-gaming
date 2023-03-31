5b278faf3f5827fe75c57c13296105a53f7a84bc
João <dev.junqueira@gmail.com>
2023-03-30 22:01:58 -0300

    :sparkles: Level 1 Done!

--- Level-1/code.py
+++ Level-1/code.py
@@ -21,13 +21,32 @@ def validorder(order: Order):
     
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            net += check_payment_input(item.amount)
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            input_validated = check_product_input(item_quantity=item.quantity, item_amount=item.amount)
+                        
+            net -= input_validated
         else:
             return("Invalid item type: %s" % item.type)
     
+    print(f"net:{net}")
     if net != 0:
         return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
     else:
         return("Order ID: %s - Full payment received!" % order.id)
+
+def check_payment_input(value: int):
+    max_reinbusement_value = -10000
+    
+    if value > max_reinbusement_value and value < 10000:
+        return value
+    else:
+        return 0
+    
+def check_product_input(item_quantity: int, item_amount: int):
+    maximum_quantity = 5
+    max_item_amount = 10000
+    
+    if item_quantity > 0 and item_quantity <= maximum_quantity and item_amount > 0 and item_amount <= max_item_amount:
+            return item_amount * item_quantity
+        
